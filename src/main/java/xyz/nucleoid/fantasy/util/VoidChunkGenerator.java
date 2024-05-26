cc2f48cd2192f0f67cf1079ef3f295f1178263d6
Inf1nityy <br0wn_rabbit@protonmail.com>
2024-05-26 18:20:30 +0200

    it works...?

--- src/main/java/xyz/nucleoid/fantasy/util/VoidChunkGenerator.java
+++ src/main/java/xyz/nucleoid/fantasy/util/VoidChunkGenerator.java
@@ -1,64 +1,87 @@
 package xyz.nucleoid.fantasy.util;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.structure.StructureManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.*;
+import net.minecraft.util.registry.RegistryLookupCodec;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.biome.source.BiomeArray;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.biome.source.FixedBiomeSource;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ProtoChunk;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.StructuresConfig;
-import net.minecraft.world.gen.feature.StructureFeature;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.Heightmap;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.gen.StructureAccessor;
 
 import java.util.Collections;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public class VoidChunkGenerator extends ChunkGenerator {
-    public VoidChunkGenerator(BiomeSource biomeSource, StructuresConfig structuresConfig) {
-        super(biomeSource, structuresConfig);
+    public static final Codec<VoidChunkGenerator> CODEC = RegistryLookupCodec.of(Registry.BIOME_KEY)
+            .xmap(VoidChunkGenerator::new, VoidChunkGenerator::getBiomeRegistry).stable().codec();
+
+    private final Registry<Biome> biomeRegistry;
+
+    public VoidChunkGenerator(Registry<Biome> biomeRegistry) {
+        super(new FixedBiomeSource(biomeRegistry.getOrThrow(BiomeKeys.THE_VOID)), new StructuresConfig(Optional.empty(), Collections.emptyMap()));
+        this.biomeRegistry = biomeRegistry;
+    }
+
+    private Registry<Biome> getBiomeRegistry() {
+        return this.biomeRegistry;
     }
 
     @Override
     protected Codec<? extends ChunkGenerator> getCodec() {
-        return null;
+        return CODEC;
     }
 
     @Override
     public ChunkGenerator withSeed(long seed) {
-        return null;
+        return this; // Seed does not matter for a void world
     }
 
     @Override
     public void buildSurface(ChunkRegion region, Chunk chunk) {
+        // No surface to build in a void world
+    }
 
+    @Override
+    public void populateBiomes(Registry<Biome> biomeRegistry, Chunk chunk) {
+        ChunkPos chunkPos = chunk.getPos();
+        ((ProtoChunk)chunk).setBiomes(new BiomeArray(this.biomeRegistry, chunkPos, this.biomeSource));
+
+        for (Biome biome : this.getBiomeSource().getBiomes()) {
+            System.out.println(biome.getClass());
+        }
     }
 
     @Override
     public void populateNoise(WorldAccess world, StructureAccessor accessor, Chunk chunk) {
-
+        // No noise to populate in a void world
     }
 
     @Override
     public int getHeight(int x, int z, Heightmap.Type heightmapType) {
-        return 0;
+        return 0; // No height in a void world
     }
 
     @Override
     public BlockView getColumnSample(int x, int z) {
         return null;
     }
+
+    @Override
+    public void generateFeatures(ChunkRegion region, StructureAccessor accessor) {
+        // no features to generate in a void world
+    }
 }
+
